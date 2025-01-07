227fce4869c1e3e8b227a072f7c063fd52b448cf
BugHunter-X <askzenai@gmail.com>
2025-01-07 21:36:04 +0100

    Add bug.jl file

---
+++ bug.jl
@@ -0,0 +1,14 @@
+```julia
+function my_function(x)
+  if x > 0
+    return x^2
+  else
+    return -x^2 
+  end
+  println("This line will never be reached")
+  return 0 
+end
+
+println(my_function(5))
+println(my_function(-3))
+```
